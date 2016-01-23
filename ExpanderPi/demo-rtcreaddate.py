e9afd3a7da7d9e5eb11e37c91163b4e0b0cd3596
Volker Moesker <moeskerv@gmail.com>
2016-01-23 19:47:25 +0100

    read rtc time once every second

---
+++ ExpanderPi/demo-rtcreaddate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+from ABE_ExpanderPi import RTC
+from ABE_helpers import ABEHelpers
+import time
+
+"""
+================================================
+ABElectronics Expander Pi |  Set Time Demo
+Version 1.0 Created 21/08/2014
+
+run with: python demo-rtcset_date.py
+===============================================
+
+This demo shows how to set the time on the Expander Pi real-time clock
+and then read the current time at 1 second intervals
+"""
+
+
+i2c_helper = ABEHelpers()
+bus = i2c_helper.get_smbus()
+rtc = RTC(bus)  # create a new instance of the RTC class
+
+while True:
+    # read the date from the RTC in ISO 8601 format and print it to the screen
+    print rtc.read_date()
+    time.sleep(1)  # wait 1 second
